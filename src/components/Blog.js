f1eb3922545853d777ece0a2ceeece9f89debeb4
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-24 22:02:27 +0600

    blog updated

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React from "react";
 
-const Blog = () => {
-    return (
-        <div>
-            
+const Blog = ({ blog }) => {
+  const { category, title, author, img, a_img, date, time } = blog;
+  return (
+    <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+      <div className="flex-shrink-0">
+        <img className="h-48 w-full object-cover" src={img} alt="" />
+      </div>
+
+      <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+        <div className="flex-1">
+          <p className="text-sm font-medium text-indigo-600">
+            <span className="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-indigo-100 text-indigo-800">
+              {category}
+            </span>
+          </p>
+          <a href="/" className="block mt-1">
+            <p className="text-xl font-semibold text-gray-900">{title}</p>
+          </a>
         </div>
-    );
+        <div className="mt-6 flex items-center">
+          <div className="flex-shrink-0">
+            <img className="h-10 w-10 rounded-full" src={a_img} alt="" />
+          </div>
+          <div className="ml-3">
+            <p className="text-sm font-medium text-gray-900 hover:underline">
+              {author}
+            </p>
+            <div className="flex space-x-1 text-sm text-gray-500">
+              <time>{date}</time>
+              <span aria-hidden="true">&middot;</span>
+              <span> {time} </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,11 +1,46 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import icon from "../assets/favicon.png";
+import search from "../assets/search.svg";
+import Blog from "./Blog";
 const Blogs = () => {
   const state = useSelector((state) => state);
-  console.log(state);
   return (
     <div>
-      <div>hi</div>
+      <nav className="bg-slate-100 shadow-md">
+        <div className="max-w-7xl mx-auto px-5 lg:px-0 flex justify-between py-3 items-center">
+          <img className="h-10" src={icon} alt="Learn with Sumit" />
+        </div>
+      </nav>
+      {/* <!-- search --> */}
+      <div className="border mt-6 border-slate-200 flex items-center w-11/12 lg:w-1/2 mx-auto bg-gray-50 h-12 px-5 rounded-lg text-sm ring-emerald-200">
+        <input
+          className="outline-none border-none bg-gray-50 h-full w-full mr-2"
+          type="search"
+          name="search"
+          placeholder="Search"
+        />
+        <img className="inline h-6 cursor-pointer" src={search} alt="Search" />
+      </div>
+      <section className="relative bg-gray-50 pt-8 pb-20 px-4 sm:px-6 lg:pt-16 lg:pb-16 lg:px-8">
+        <div className="relative max-w-7xl mx-auto">
+          <div className="text-center">
+            <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
+              ALL BLOGS ARE HERE
+            </h2>
+            <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa
+              libero labore natus atque, ducimus sed.
+            </p>
+          </div>
+          {/* <!-- card grid  --> */}
+          <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+
+           {state.map((blog)=>{ return<Blog blog={blog} key={blog.id}/>})}
+         
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
